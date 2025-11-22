fcb308f737c1139a6218ec2c1f245997f81e08f9
Saurabh Tripathi <saurabhofficial8840@gmail.com>
2025-11-22 18:52:22 +0530

    Initial project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+
+export default async function Home() {
+
+  return <div>
+    <h1 className="flex justify-center items-center h-screen">heelo button </h1>
+    <Button>button</Button>
+  </div>
+ 
+}
