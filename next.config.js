43d07af8ec80d561bd9931e6571201634d8ec605
Saurabh Tripathi <saurabhofficial8840@gmail.com>
2025-11-23 19:28:45 +0530

    update file

--- next.config.js
+++ next.config.js
@@ -1,20 +1,21 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
-    typescript:{
-        ignoreBuildErrors: true,
-    },
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-    experimental: {
-    optimizeCss: false, 
-  }
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    optimizeCss: false,         // Disable LightningCSS
+    // @ts-ignore
+    optimizePackageImports: false,
+  },
+  tailwind: {
+    disableOxide: true,         // Disable Tailwind native binaries
+  },
 };
 
 export default config;
