71194597792188f7665d782fedb2633c43e202ff
Saurabh Tripathi <saurabhofficial8840@gmail.com>
2025-12-01 23:19:08 +0530

    update db file

--- src/server/db.ts
+++ src/server/db.ts
@@ -14,3 +14,9 @@ const globalForPrisma = globalThis as unknown as {
 export const db = globalForPrisma.prisma ?? createPrismaClient();
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+
+
+
+//we are using this file bcz when we use prisma client directly in the api route files it creates multiple instances of 
+//prisma client in development mode causing issues with connections to the database 
+//due to hot reloading feature of nextjs 
